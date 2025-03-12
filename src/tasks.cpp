646a662f6bd0a3d1939f37c2f55bd46e333fb093
KADCHDR <125730153+KADCHDR@users.noreply.github.com>
2025-03-12 19:24:15 +0300

    Create tasks.cpp

---
+++ src/tasks.cpp
@@ -0,0 +1,23 @@
+#include "tasks.h"
+#include "circle.h"
+
+double solveRopeProblem() {
+    const double earthRadius = 6378100.0;
+    Circle earth(earthRadius);
+    earth.setFerence(earth.getFerence() + 1.0);
+    return earth.getRadius() - earthRadius;
+}
+
+PoolResult calculatePoolCost() {
+    const double poolRadius = 3.0;
+    const double width = 1.0;
+    const double outerRadius = poolRadius + width;
+
+    Circle pool(poolRadius);
+    Circle outer(outerRadius);
+
+    PoolResult result;
+    result.concreteCost = (outer.getArea() - pool.getArea()) * 1000.0;
+    result.fenceCost = outer.getFerence() * 2000.0;
+    return result;
+}
