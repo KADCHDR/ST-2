60bc73af72b7f2d1610da46539ca1350aed5d66a
KADCHDR <125730153+KADCHDR@users.noreply.github.com>
2025-04-14 23:20:11 +0300

    1

--- include/circle.h
+++ include/circle.h
@@ -1,22 +1,32 @@
 // Copyright 2022 UNN-CS
+
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
+const double PI = 3.1415926535;
+
 class Circle {
  private:
-    double radius;
-    double ference;
-    double area;
+  double radius;
+  double ference;
+  double area;
+
+  void updateFerence();
+  void updateArea();
+  void validateRadius(double);
 
  public:
-    explicit Circle(double radius);
-    void setRadius(double r);
-    void setFerence(double f);
-    void setArea(double a);
-    double getRadius() const;
-    double getFerence() const;
-    double getArea() const;
+  explicit Circle(double radius);
+
+  void setRadius(double radius);
+  void setFerence(double ference);
+  void setArea(double area);
+
+  double getRadius() const;
+  double getFerence() const;
+  double getArea() const;
 };
 
 #endif  // INCLUDE_CIRCLE_H_
+#endif // INCLUDE_CIRCLE_H_
